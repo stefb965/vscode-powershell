dc9b6d7bc8ece34d07add406841797341a83baf8
David Wilson <daviwil@microsoft.com>
2016-01-10 16:36:00 -0800

    Add choice prompt handling support
    This change adds choice prompt handling support by responding to the new
    powerShell/showChoicePrompt message.  When received, the extension will
    show a quick pick list with the possible choices.  If the user selects an
    option with 'enter' it will be sent for processing.  If the quick pick
    gets cancelled either by pressing `ESC` or the UI losing focus, the prompt
    will be cancelled in PowerShell.

--- src/features/Console.ts
+++ src/features/Console.ts
@@ -1,9 +1,8 @@
 import vscode = require('vscode');
-import { LanguageClient } from 'vscode-languageclient';
-import { RequestType, NotificationType, ResponseError } from 'vscode-jsonrpc';
+import { LanguageClient, RequestType, NotificationType } from 'vscode-languageclient';
 
 export namespace EvaluateRequest {
-	export const type: RequestType<EvaluateRequestArguments, void, void> = 
+	export const type: RequestType<EvaluateRequestArguments, void, void> =
 		{ get method() { return 'evaluate'; } };
 }
 
@@ -12,7 +11,7 @@ export interface EvaluateRequestArguments {
 }
 
 export namespace OutputNotification {
-	export const type: NotificationType<OutputNotificationBody> = 
+	export const type: NotificationType<OutputNotificationBody> =
 		{ get method() { return 'output'; } };
 }
 
@@ -21,8 +20,80 @@ export interface OutputNotificationBody {
 	output: string;
 }
 
+export namespace ShowChoicePromptNotification {
+	export const type: NotificationType<ShowChoicePromptNotificationBody> =
+		{ get method() { return 'powerShell/showChoicePrompt'; } };
+}
+
+interface ChoiceDetails {
+    label: string;
+    helpMessage: string;
+}
+
+interface ShowChoicePromptNotificationBody {
+	caption: string;
+	message: string;
+    choices: ChoiceDetails[];
+    defaultChoice: number;
+}
+
+export namespace CompleteChoicePromptNotification {
+	export const type: NotificationType<CompleteChoicePromptNotificationBody> =
+		{ get method() { return 'powerShell/completeChoicePrompt'; } };
+}
+
+interface CompleteChoicePromptNotificationBody {
+    chosenItem: string;
+    promptCancelled: boolean;
+}
+
+function showChoicePrompt(
+    promptDetails: ShowChoicePromptNotificationBody,
+    client: LanguageClient) {
+
+    var quickPickItems =
+        promptDetails.choices.map<vscode.QuickPickItem>(choice => {
+            return {
+                label: choice.label,
+                description: choice.helpMessage
+            }
+        });
+
+    // Shift the default item to the front of the
+    // array so that the user can select it easily
+    if (promptDetails.defaultChoice > -1 &&
+        promptDetails.defaultChoice < promptDetails.choices.length) {
+
+        var defaultChoiceItem = quickPickItems[promptDetails.defaultChoice];
+        quickPickItems.splice(promptDetails.defaultChoice, 1);
+
+        // Add the default choice to the head of the array
+        quickPickItems = [defaultChoiceItem].concat(quickPickItems);
+    }
+
+    vscode.window
+        .showQuickPick(
+            quickPickItems,
+            { placeHolder: promptDetails.caption + " - " + promptDetails.message })
+        .then(chosenItem => onItemSelected(chosenItem, client));
+}
+
+function onItemSelected(chosenItem: vscode.QuickPickItem, client: LanguageClient) {
+    if (chosenItem !== undefined) {
+        client.sendNotification(
+            CompleteChoicePromptNotification.type,
+            { chosenItem: chosenItem.label });
+    }
+    else {
+        // User cancelled the prompt, send the cancellation
+        client.sendNotification(
+            CompleteChoicePromptNotification.type,
+            { promptCancelled: true });
+    }
+}
+
 export function registerConsoleCommands(client: LanguageClient): void {
-	
+
 	vscode.commands.registerCommand('PowerShell.RunSelection', () => {
 		var editor = vscode.window.activeTextEditor;
 		var start = editor.selection.start;
@@ -31,19 +102,23 @@ export function registerConsoleCommands(client: LanguageClient): void {
 	            start = new vscode.Position(start.line, 0)
 	        }
         	client.sendRequest(EvaluateRequest.type, {
-			expression: 
+			expression:
 				editor.document.getText(
 					new vscode.Range(start, end))
-		});		
+		});
 	});
-	
+
 	var consoleChannel = vscode.window.createOutputChannel("PowerShell Output");
 	client.onNotification(OutputNotification.type, (output) => {
 		var outputEditorExist = vscode.window.visibleTextEditors.some((editor) => {
-	           return editor.document.languageId == 'Log'  
+	           return editor.document.languageId == 'Log'
 	        });
 	        if(!outputEditorExist)
-			consoleChannel.show(vscode.ViewColumn.Three);		
+			consoleChannel.show(vscode.ViewColumn.Three);
 		consoleChannel.append(output.output);
 	});
+
+    client.onNotification(
+        ShowChoicePromptNotification.type,
+        promptDetails => showChoicePrompt(promptDetails, client));
 }
--- src/features/ExpandAlias.ts
+++ src/features/ExpandAlias.ts
@@ -1,6 +1,5 @@
 import vscode = require('vscode');
-import { LanguageClient } from 'vscode-languageclient';
-import { RequestType, NotificationType, ResponseError } from 'vscode-jsonrpc';
+import { LanguageClient, RequestType, NotificationType } from 'vscode-languageclient';
 import Window = vscode.window;
 
 export namespace ExpandAliasRequest {
@@ -11,28 +10,28 @@ export function registerExpandAliasCommand(client: LanguageClient): void {
     var disposable = vscode.commands.registerCommand('PowerShell.ExpandAlias', () => {
 
 		var editor    = Window.activeTextEditor;
-		var document  = editor.document;		
-		var selection = editor.selection;	
+		var document  = editor.document;
+		var selection = editor.selection;
 		var text, range;
-		
+
 		var sls = selection.start;
-		var sle = selection.end; 
-		
+		var sle = selection.end;
+
 		if(
 			(sls.character === sle.character ) &&
 			(sls.line === sle.line)
 		) {
-			text  = document.getText();		
-			range = new vscode.Range(0, 0, document.lineCount, text.length);		
+			text  = document.getText();
+			range = new vscode.Range(0, 0, document.lineCount, text.length);
 		} else {
-			text  = document.getText(selection);		
-			range = new vscode.Range(sls.line, sls.character, sle.line, sle.character);		
+			text  = document.getText(selection);
+			range = new vscode.Range(sls.line, sls.character, sle.line, sle.character);
 		}
-						
-		client.sendRequest(ExpandAliasRequest.type, text).then((result) => {		
-			editor.edit((editBuilder) => {				
+
+		client.sendRequest(ExpandAliasRequest.type, text).then((result) => {
+			editor.edit((editBuilder) => {
 				editBuilder.replace(range, result);
 			});
-		});	
+		});
     });
 }
