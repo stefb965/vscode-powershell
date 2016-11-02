96b75484a786e78b394bb0311de057135b30a6ba
David Wilson <daviwil@microsoft.com>
2016-11-02 12:03:56 -0700

    FIXUP: Merge with IFeature definition

--- src/feature.ts
+++ src/feature.ts
@@ -8,4 +8,5 @@ export { LanguageClient } from 'vscode-languageclient';
 
 export interface IFeature extends vscode.Disposable {
     setLanguageClient(languageclient: LanguageClient);
+    dispose();
 }
